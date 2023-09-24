415a18b779946d41fd7b2878e1c8572ba5965037
Seven <graf.lancelot7@gmail.com>
2023-09-25 00:37:13 +0300

    add block horisont

--- src/index.js
+++ src/index.js
@@ -8,16 +8,23 @@ const refs = {
 
 document.addEventListener('DOMContentLoaded', () => {
   if (screen.orientation.type === 'portrait-primary') {
-    alert(false);
+    refs.modal.style.display = 'none';
+    document.querySelector('main').style.display = 'none';
+    document.body.classList.add('wrong');
   }
 });
 screen.orientation.addEventListener('change', event => {
   const type = event.target.type;
   const angle = event.target.angle;
   if (type === 'portrait-primary') {
-    alert(false);
+    refs.modal.style.display = 'none';
+    document.querySelector('main').style.display = 'none';
+    document.body.classList.add('wrong');
+  } else {
+    document.body.classList.remove('wrong');
+    refs.modal.style.display = 'block';
+    document.querySelector('main').style.display = 'block';
   }
-  console.log(`ScreenOrientation change: ${type}, ${angle} degrees.`);
 });
 
 refs.openModal.addEventListener('click', toggleOpenModal);
