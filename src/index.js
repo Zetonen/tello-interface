6f628be8498dd7a241a0ddd6ed66a6c409a8d7cc
Seven <graf.lancelot7@gmail.com>
2023-09-24 22:14:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+const refs = {
+  modal: document.querySelector('.modal'),
+  openModal: document.querySelector('.mode'),
+  closeModal: document.querySelector('.modal__close'),
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (screen.orientation.type === 'portrait-primary') {
+    alert(false);
+  }
+});
 screen.orientation.addEventListener('change', event => {
   const type = event.target.type;
   const angle = event.target.angle;
-  if (type ==='portrait-primary') {
-    alert(false)
-    console.log(true);
+  if (type === 'portrait-primary') {
+    alert(false);
   }
   console.log(`ScreenOrientation change: ${type}, ${angle} degrees.`);
 });
+
+refs.openModal.addEventListener('click', toggleOpenModal);
+refs.closeModal.addEventListener('click', toggleOpenModal);
+
+function toggleOpenModal() {
+  refs.modal.classList.toggle('hidden');
+}
