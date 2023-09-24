e7fb705e71118fd3ddb3b9de7a094e00ab489461
Zetonen <76436489+Zetonen@users.noreply.github.com>
2023-09-24 09:18:12 +0000

    Deploying to gh-pages from @ Zetonen/tello-interface@1569cc5a12bb1a5b78fc29bc4f7b5f4f79adb418 🚀

---
+++ index.4d0cd4e0.js
@@ -0,0 +1,2 @@
+screen.orientation.addEventListener("change",(function(e){var n=e.target.type,a=e.target.angle;"landscape-primary"===n&&(alert(!0),console.log(!0)),console.log("ScreenOrientation change: ".concat(n,", ").concat(a," degrees."))}));
+//# sourceMappingURL=index.4d0cd4e0.js.map
